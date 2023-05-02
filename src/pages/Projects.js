b637ce784f12123da7bc623bb9f14d8b0abc8d8d
jhadevansh0809 <jhadevansh0809@gmail.com>
2023-05-02 19:09:40 +0530

    Adding a skill and a project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,70 +1,101 @@
-import React,{useEffect} from 'react'
-import carzone from '../images/carzone.jpg'
-import cricblogs from '../images/cricblogs.jpg'
-import youtube from '../images/youtube.jpg'
-import zigbee from '../images/zigbee.jpg'
+import React, { useEffect } from "react";
+import carzone from "../images/carzone.jpg";
+import cricblogs from "../images/cricblogs.jpg";
+import rasoi from "../images/rasoi.png";
+import zigbee from "../images/zigbee.jpg";
 
 const Projects = () => {
-
   useEffect(() => {
-      document.title = 'Devansh Jha | Projects';
+    document.title = "Devansh Jha | Projects";
   }, []);
-  
+
   return (
-    <div className='project-container w-2/5 text-white m-auto my-20 sm:w-4/5 lg:w-3/5'>
-       <h3 className='text-center mb-10 text-[30px]'>My Projects</h3>
+    <div className="project-container w-2/5 text-white m-auto my-20 sm:w-4/5 lg:w-3/5">
+      <h3 className="text-center mb-10 text-[30px]">My Projects</h3>
       <div>
-        <img src={carzone} alt='car dealer'/>
-        <h3 className='text-[25px]'>Carzone: A Car Selling Website</h3>
-        <span className='text-blue-700 block'>Web</span>
+        <img src={carzone} alt="car dealer" />
+        <h3 className="text-[25px]">Carzone: A Car Selling Website</h3>
+        <span className="text-blue-700 block">Web</span>
         <span>06/2021-07/2021</span>
-        <h5 className='mt-4'>Description:</h5>
-        <p className='mb-2'>A responsive website providing all the functionalities of a car dealer website from which a user can buy a car, mainly a second hand car.</p>
+        <h5 className="mt-4">Description:</h5>
+        <p className="mb-2">
+          A responsive website providing all the functionalities of a car dealer
+          website from which a user can buy a car, mainly a second hand car.
+        </p>
         <h5>Technology:</h5>
-        <p className='mb-2'>HTML, CSS, Javascript, Django</p>
+        <p className="mb-2">HTML, CSS, Javascript, Django</p>
         <h5>Role:</h5>
-        <p className='mb-5'>Backend</p>
-        <a href='https://github.com/jhadevansh0809/carzoneservices' target='_blank'><button className='btn mb-2'>Code Link</button></a>
-        <a href='https://carzoneservices.pythonanywhere.com/' target='_blank'><button className='btn mb-2 ml-3 btn-red'>Live Link</button></a>
+        <p className="mb-5">Backend</p>
+        <a
+          href="https://github.com/jhadevansh0809/carzoneservices"
+          target="_blank"
+        >
+          <button className="btn mb-2">Code Link</button>
+        </a>
+        <a href="https://carzoneservices.pythonanywhere.com/" target="_blank">
+          <button className="btn mb-2 ml-3 btn-red">Live Link</button>
+        </a>
       </div>
-      <div className='mt-10'>
-        <img src={cricblogs} alt='cricket'/>
-        <h3 className='text-[25px]'>CricBlogs: A Cricket Blog Website</h3>
-        <span className='text-blue-700 block'>Web</span>
+      <div className="mt-10">
+        <img src={rasoi} alt="rasoi page" />
+        <h3 className="text-[25px]">Rasoi: A Food Ordering Website</h3>
+        <span className="text-blue-700 block">Web</span>
+        <span>03/2023 – 04/2023</span>
+        <h5 className="mt-4">Description:</h5>
+        <p className="mb-2">
+          A food ordering website with functionalitieslike Add to cart, Payment
+          Gateway along with interactive Admin Panel.
+        </p>
+        <h5>Technology:</h5>
+        <p className="mb-2">HTML, Tailwind CSS, NextJS</p>
+        <a href="https://github.com/jhadevansh0809/Rasoi" target="_blank">
+          <button className="btn mb-2">Code Link</button>
+        </a>
+        <a href="https://myrasoi.vercel.app/" target="_blank">
+          <button className="btn mb-2 ml-3 btn-red">Live Link</button>
+        </a>
+      </div>
+      <div className="mt-10">
+        <img src={cricblogs} alt="cricket" />
+        <h3 className="text-[25px]">CricBlogs: A Cricket Blog Website</h3>
+        <span className="text-blue-700 block">Web</span>
         <span>03/2023-03/2023</span>
-        <h5 className='mt-4'>Description:</h5>
-        <p className='mb-2'>A blog website which allow users to add, edit and remove blogs. Used Django REST Framework to create RESTful API endpoints and fetched data using ReactJS. Implemented user authentication functionality using JWT(JSON Web Token).</p>
+        <h5 className="mt-4">Description:</h5>
+        <p className="mb-2">
+          A blog website which allow users to add, edit and remove blogs. Used
+          Django REST Framework to create RESTful API endpoints and fetched data
+          using ReactJS. Implemented user authentication functionality using
+          JWT(JSON Web Token).
+        </p>
         <h5>Technology:</h5>
-        <p className='mb-5'>HTML, CSS, ReactJS, Django REST Framework</p>
-        <a href='https://github.com/jhadevansh0809/CricBlogs-FE' target='_blank'><button className='btn mb-2'>Code Link</button></a>
-        <a href='https://eclectic-cajeta-c28f98.netlify.app/' target='_blank'><button className='btn mb-2 ml-3 btn-red'>Live Link</button></a>
+        <p className="mb-5">HTML, CSS, ReactJS, Django REST Framework</p>
+        <a
+          href="https://github.com/jhadevansh0809/CricBlogs-FE"
+          target="_blank"
+        >
+          <button className="btn mb-2">Code Link</button>
+        </a>
+        <a href="https://eclectic-cajeta-c28f98.netlify.app/" target="_blank">
+          <button className="btn mb-2 ml-3 btn-red">Live Link</button>
+        </a>
       </div>
-      <div className='mt-10'>
-        <img src={youtube} alt='youtube logo'/>
-        <h3 className='text-[25px]'>YTube: A YouTube Clone</h3>
-        <span className='text-blue-700 block'>Web</span>
-        <span>02/2023-03/2023</span>
-        <h5 className='mt-4'>Description:</h5>
-        <p className='mb-2'>A responsive clone that aims to replicate the essential features and functionalities of the popular video-sharing platform, Youtube.</p>
-        <h5>Technology:</h5>
-        <p className='mb-2'>HTML, CSS, ReactJS</p>
-        <a href='https://github.com/jhadevansh0809/YTube' target='_blank'><button className='btn mb-2'>Code Link</button></a>
-        <a href='https://beamish-brioche-539613.netlify.app/' target='_blank'><button className='btn mb-2 ml-3 btn-red'>Live Link</button></a>
-      </div>
-      <div className='mt-10'>
-        <img src={zigbee} alt='car dealer'/>
-        <h3 className='text-[25px]'>Automation Using Zigbee</h3>
-        <span className='text-blue-700 block'>IoT</span>
+      <div className="mt-10">
+        <img src={zigbee} alt="car dealer" />
+        <h3 className="text-[25px]">Automation Using Zigbee</h3>
+        <span className="text-blue-700 block">IoT</span>
         <span>02/2022-04/2022</span>
-        <h5 className='mt-4'>Description:</h5>
-        <p className='mb-2'>A wireless network with star topology using zigbee which works on the principle of low data rate and long range communication.</p>
+        <h5 className="mt-4">Description:</h5>
+        <p className="mb-2">
+          A wireless network with star topology using zigbee which works on the
+          principle of low data rate and long range communication.
+        </p>
         <h5>Technology:</h5>
-        <p className='mb-2'>Arduino Programming, Zigbee configuration</p>
+        <p className="mb-2">Arduino Programming, Zigbee configuration</p>
         <h5>Role:</h5>
         <p>Arduino Programming</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,64 +1,73 @@
-import React,{useEffect} from 'react'
-import c from '../images/c.png'
-import cpp from '../images/cpp.png'
-import python from '../images/python.png'
-import js from '../images/js.png'
-import html from '../images/html.png'
-import css from '../images/css.png'
-import tailwind from '../images/tailwind.png'
-import react from '../images/react.png'
-import django from '../images/django.png'
-import git from '../images/git.png'
-import vscode from '../images/vscode.png'
-import pycharm from '../images/pycharm.png'
-import arduino from '../images/arduino.png'
-import iot from '../images/iot.png'
+import React, { useEffect } from "react";
+import c from "../images/c.png";
+import cpp from "../images/cpp.png";
+import python from "../images/python.png";
+import js from "../images/js.png";
+import html from "../images/html.png";
+import css from "../images/css.png";
+import tailwind from "../images/tailwind.png";
+import react from "../images/react.png";
+import django from "../images/django.png";
+import nextjs from "../images/nextjs.png";
+import git from "../images/git.png";
+import vscode from "../images/vscode.png";
+import pycharm from "../images/pycharm.png";
+import arduino from "../images/arduino.png";
+import iot from "../images/iot.png";
 
 const Skills = () => {
-   
-   useEffect(() => {
-        document.title = 'Devansh Jha | Skills';
-   }, []);
+  useEffect(() => {
+    document.title = "Devansh Jha | Skills";
+  }, []);
 
   return (
-      <div className='skills-container w-4/5 m-auto my-20'>
-       <h3 className='text-center mb-10 text-[30px]'>My Skills</h3>
-        <div className='my-10'>
-        <p className='font-bold text-orange-600 text-center mb-1 uppercase text-md'>Programming Languages</p>
-         <div className='py-10 flex items-center justify-center md:flex-wrap'>
-            <img src={c} alt='c-programming logo'/>
-            <img src={cpp} alt='c++ programming logo'/>
-            <img src={python} alt='python programming logo'/>
-            <img src={js} alt='javascript-programming logo'/>
-         </div>
-        </div>
-        <div className='my-10'>
-        <p className='font-bold text-orange-700 text-center mb-1 uppercase text-md'>Web Development</p>
-         <div className='py-10 flex items-center justify-center md:flex-wrap'>
-            <img src={html} alt='html logo'/>
-            <img src={css} alt='css logo'/>
-            <img src={tailwind} alt='tailwind css logo'/>
-            <img src={react} alt='react logo'/>
-            <img src={django} alt='django logo'/>
-         </div>
-        </div>
-        <div className='my-10'>
-        <p className='font-bold text-orange-700 text-center mb-1 uppercase text-md'>Tools</p>
-         <div className='py-10 flex items-center justify-center md:flex-wrap'>
-            <img src={git} alt='git logo'/>
-            <img src={vscode} alt='vscode logo'/>
-            <img src={pycharm} alt='pycharm logo'/>
-         </div>
-        </div>
-        <div className='my-10'>
-        <p className='font-bold text-orange-700 text-center mb-1 uppercase text-md'>Internet of Things</p>
-         <div className='py-10 flex items-center justify-center md:flex-wrap'>
-            <img src={arduino} alt='arduino logo'/>
-            <img src={iot} alt='iot logo'/>
-         </div>
+    <div className="skills-container w-4/5 m-auto my-20">
+      <h3 className="text-center mb-10 text-[30px]">My Skills</h3>
+      <div className="my-10">
+        <p className="font-bold text-orange-600 text-center mb-1 uppercase text-md">
+          Programming Languages
+        </p>
+        <div className="py-10 flex items-center justify-center md:flex-wrap">
+          <img src={c} alt="c-programming logo" />
+          <img src={cpp} alt="c++ programming logo" />
+          <img src={python} alt="python programming logo" />
+          <img src={js} alt="javascript-programming logo" />
         </div>
       </div>
-  )
-}
+      <div className="my-10">
+        <p className="font-bold text-orange-700 text-center mb-1 uppercase text-md">
+          Web Development
+        </p>
+        <div className="py-10 flex items-center justify-center flex-wrap">
+          <img src={html} alt="html logo" />
+          <img src={css} alt="css logo" />
+          <img src={tailwind} alt="tailwind css logo" />
+          <img src={react} alt="react logo" />
+          <img src={django} alt="django logo" />
+          <img src={nextjs} alt="nextjs logo" />
+        </div>
+      </div>
+      <div className="my-10">
+        <p className="font-bold text-orange-700 text-center mb-1 uppercase text-md">
+          Tools
+        </p>
+        <div className="py-10 flex items-center justify-center md:flex-wrap">
+          <img src={git} alt="git logo" />
+          <img src={vscode} alt="vscode logo" />
+          <img src={pycharm} alt="pycharm logo" />
+        </div>
+      </div>
+      <div className="my-10">
+        <p className="font-bold text-orange-700 text-center mb-1 uppercase text-md">
+          Internet of Things
+        </p>
+        <div className="py-10 flex items-center justify-center md:flex-wrap">
+          <img src={arduino} alt="arduino logo" />
+          <img src={iot} alt="iot logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
